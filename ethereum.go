63ae35d0487acb21393f64ee9c0f38875b70c6e2
lpopo0856 <lpopo0856@gmail.com>
2023-03-29 12:35:05 +0800

    Update testnet from Goerli to Sepolia

--- ethereum.go
+++ ethereum.go
@@ -30,7 +30,7 @@ type Wallet struct {
 
 // NewWallet creates a wallet from a given seed
 func NewWallet(seed []byte, network, rpcURL string) (*Wallet, error) {
-	chainID := params.GoerliChainConfig.ChainID
+	chainID := params.SepoliaChainConfig.ChainID
 	if network == "livenet" {
 		chainID = params.MainnetChainConfig.ChainID
 	}
@@ -61,7 +61,7 @@ func NewWallet(seed []byte, network, rpcURL string) (*Wallet, error) {
 
 // NewWallet creates a wallet from a given mnemonic phrases
 func NewWalletFromMnemonic(words, network, rpcURL string) (*Wallet, error) {
-	chainID := params.RinkebyChainConfig.ChainID
+	chainID := params.SepoliaChainConfig.ChainID
 	if network == "livenet" {
 		chainID = params.MainnetChainConfig.ChainID
 	}
